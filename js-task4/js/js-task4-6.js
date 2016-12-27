cb269cb20aa13fb3fb2f6a646094d1051c6a546c
husihao <husihao1994@gmail.com>
2016-12-27 22:06:47 +0800

    上传代码js-task4

--- js-task4/js/js-task4-6.js
+++ js-task4/js/js-task4-6.js
@@ -11,8 +11,14 @@ var arrDead = save.arrD;
 var winner =  save.win;
 var identity = document.getElementById("identity");
 var day = Number(save.date);
+// 当第一天杀手就胜利时，页面不会跳到黑夜解密页，日期数据就不会保存下来，所以判断save.day为undefined时，给day赋值1.
+if(save.date == undefined){
+    day=1;
+}
+console.log(day);
 console.log(save);
 
+// 判断杀手还是平民胜利
 if( winner=="x"){
     identity.innerHTML="杀手";
 }
@@ -27,15 +33,17 @@ y.innerHTML=arrY.length+1;
 
 // var latestDead = arrDead[arrDead.length-1];
 
-function arrDead(i){
-    if (arrX.indexOf(i)!= -1){
-        return "杀手";
+// 成立一个新的死亡数组,角色身份与死亡数组一一对应
+var deadPlayer=[];
+
+for(i=0; i<arrDead.length;i++){
+    if (arrX.indexOf(arrDead[i])!= -1){
+        deadPlayer.push("杀手");
     }
     else {
-        return "平民";
+        deadPlayer.push("平民");
     }
 }
-
 
 
 var deadNote= document.getElementById("dead-note");
@@ -44,7 +52,19 @@ if(arrDead.length%2==0){
     for(var i=1; i<=day; i++){
         var content = document.createElement("div");
         content.className="day1" ;
-        content.innerHTML= '<p> <span class="p3">第'+i+'天</span> </p> <p class="p5"> <span>晚上：'+ arrDead[2*i-2]  +'号被杀手杀死，'+arrDead[2*i-2]+'号是'+arrDead(arrDead[2*i-2])+'</span> <br> <span>白天：'+arrDead[2*i-1]+'号被全民投票投死，'+arrDead[2*i-1]+'号是'+arrDead(arrDead[2*i-1])+'</span> </p>';
+        content.innerHTML= '<p> <span class="p3">第'+i+'天</span> </p> <p class="p5"> <span>晚上：'
+            + arrDead[2*i-2]
+            +'号被杀手杀死，'
+            +arrDead[2*i-2]+
+            '号是'
+            +deadPlayer[2*i-2]+
+            '</span> <br> <span>白天：'
+            +arrDead[2*i-1]+'' +
+            '号被全民投票投死，'
+            +arrDead[2*i-1]+'' +
+            '号是'
+            + deadPlayer[2*i-1]+
+            '</span> </p>';
 
         deadNote.appendChild(content);
     }
@@ -53,16 +73,40 @@ else{
     for(var i=1; i<day; i++){
         var content = document.createElement("div");
         content.className="day1" ;
-        content.innerHTML= '<p> <span class="p3">第'+i+'天</span> </p> <p class="p5"> <span>晚上：'+ arrDead[2*i-2]  +'号被杀手杀死，'+arrDead[2*i-2]+'号是'+arrDead(arrDead[2*i-2])+'</span> <br> <span>白天：'+arrDead[2*i-1]+'号被全民投票投死，'+arrDead[2*i-1]+'号是'+arrDead(arrDead[2*i-1])+'</span> </p>';
+        content.innerHTML= '<p> <span class="p3">第'+i+'天</span> </p> <p class="p5"> <span>晚上：'
+            + arrDead[2*i-2]  +
+            '号被杀手杀死，'
+            +arrDead[2*i-2]+
+            '号是'
+            +deadPlayer[2*i-2]+
+            '</span> <br> <span>白天：'
+            +arrDead[2*i-1]+
+            '号被全民投票投死，'
+            +arrDead[2*i-1]+
+            '号是'
+            +deadPlayer[2*i-1]+
+            '</span> </p>';
 
         deadNote.appendChild(content);
     }
 
     var content = document.createElement("div");
     content.className="day1" ;
-    content.innerHTML= '<p> <span class="p3">第'+day+'天</span> </p> <p class="p5"> <span>晚上：'+ arrDead[2*day-2]  +'号被杀手杀死，'+arrDead[2*day-2]+'号是'+arrDead(arrDead[2*day-2])+'</span> <br> ';
+    content.innerHTML= '<p> <span class="p3">第'+day+'天</span> </p> <p class="p5"> <span>晚上：'
+        + arrDead[2*day-2]  +
+        '号被杀手杀死，'
+        +arrDead[2*day-2]+
+        '号是'
+        +deadPlayer[2*day-2]+
+        '</span> <br> ';
 
     deadNote.appendChild(content);
 
 }
 
+
+var btn = document.getElementById("btn");
+btn.onclick=function () {
+    localStorage.clear();
+    location.href= "js-task4-start.html"
+}
